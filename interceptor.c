4abe56e9af222c236070026285ca48221c719582
andrewbinyu <binyu03@gmail.com>
2017-06-12 11:09:16 -0400

    add more printk

--- interceptor.c
+++ interceptor.c
@@ -440,7 +440,7 @@ asmlinkage long my_syscall(int cmd, int syscall, int pid) {
 			
 			int rr;
 			rr = check_pid_monitored(syscall, pid);
-			printk(KERN_WARNING "check if pid is in the list. rr==%d, 1 is in, 0 is not." rr);
+			printk(KERN_WARNING "check if pid is in the list. rr==%d, 1 is in, 0 is not.", rr);
         }
         else if (table[syscall].monitored == 1) {
             ret = add_pid_sysc(pid, syscall);
