53f77b3f9c18ca16f7897d4611ae6575052efe01
andrewbinyu <binyu03@gmail.com>
2017-06-12 11:54:57 -0400

    add some printk

--- interceptor.c
+++ interceptor.c
@@ -444,6 +444,13 @@ asmlinkage long my_syscall(int cmd, int syscall, int pid) {
         }
         else if (table[syscall].monitored == 1) {
             ret = add_pid_sysc(pid, syscall);
+		
+			int rr;
+			rr = check_pid_monitored(syscall, pid);
+			printk(KERN_WARNING "monitored==1, check if pid is in the list. rr==%d, 1 is in, 0 is not.", rr);
+			rr=((cmd == REQUEST_START_MONITORING) && (((check_pid_monitored(syscall, pid) == 1)&&(table[syscall].monitored == 1)) || ((check_pid_monitored(syscall,pid) == 0 ) && (table[syscall].monitored == 2))))
+			printk(KERN_WARNING "check EBUSY condition. rr= %d, 1 is busy, 0 is not", rr);
+
 			printk(KERN_WARNING "monitored==1,add pid to list, ret=%d", ret);
 			ret = add_pid_sysc(pid, syscall); // add again
 			printk(KERN_WARNING "monitored==1,add pid to list again, ret=%d", ret);
