11fbec8e2d8dee92887e081f6a5000e93d3ab5a1
andrewbinyu <binyu03@gmail.com>
2017-06-13 10:12:02 -0400

    change test_ful

--- test_full.c
+++ test_full.c
@@ -218,7 +218,7 @@ int main(int argc, char **argv) {
 	do_intercept(__NR_exit, 0);
 	do_release(__NR_exit, 0);
 
-	test_syscall(SYS_open);
+	test_syscall(__NR);
 	/* The above line of code tests SYS_open.
 	   Feel free to add more tests here for other system calls, 
 	   once you get everything to work; check Linux documentation
